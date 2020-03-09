518281c408886e9510db0f977c41513e7f47d619
panfeilei <panfeilei@guanmai.cn>
2020-03-10 00:58:19 +0800

    'g'

--- Tree.py
+++ Tree.py
@@ -91,7 +91,25 @@ def frontprintStack(root):
         stack.append(node.right)
         stack.append(node.left)
     
+def listtree(node, depth):
+    if not node or (not node.left and not node.right):
+        return depth
+
+    left = listtree(node.left, depth if node.left else 0)
+    if left == -1:
+        return -1
+    right = listtree(node.right, depth if node.right else 0)
+    if right == -1:
+        return -1
+    print(left, right)
+    return max(left , right) + 1 if abs(left - right) <= 1 else -1
+
+def isBalanced(root: TreeNode) -> bool:
+    d = listtree(root,1)
+    return d
+
 if __name__ == "__main__":
-    s = buildTree([1,2,4,None,7,None,3, 6,9])
-    midprintStack(s)
+    s = buildTree([1,2,2,3,None,None,3,4,None,None,4])
+    g = isBalanced(s)
+    print(g)
 
