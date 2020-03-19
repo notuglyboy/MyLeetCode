fbf6693afb2f0c97eb931c48e80205341fd48923
潘飞雷 <panfeilei@guanmai.cn>
2020-03-19 19:40:33 +0800

    g

--- 189. 旋转数组.py
+++ 189. 旋转数组.py
@@ -1,16 +1,22 @@
-def rotate(nums, k1):
-    k =  k1 % len(nums)
-    left = 0
-    right = len(nums) - k
-    print(left, k, right)
-    for i in range(k):
-        left_tmp = nums[left]
-        right_tmp = nums[right]
-        nums[right] = left_tmp
-        nums[left] = right_tmp
-        right +=1
-        left += 1
-        print(nums)
-nums = [1,2, 3,4,5,6,7]
-rotate(nums, 5) # [3,4,5,6,7, 1,2]
+def rotate(nums, k):
+    start = 0
+    end = len(nums) -1
+    m = k % len(nums)
+    reverse(nums, 0, end)
+    reverse(nums, 0, m-1)
+    reverse(nums, m,end)
+
+def reverse(nums, s, e):
+    print(s, e)
+    while s < e:
+
+        tmp = nums[s]
+        nums[s] = nums[e]
+        nums[e] = tmp
+        s += 1
+        e -= 1
+
+nums = [1,2]
+#reverse(nums, 0 ,6)
+rotate(nums, 2) # [3,4,5,6,7, 1,2]
 print(nums)
