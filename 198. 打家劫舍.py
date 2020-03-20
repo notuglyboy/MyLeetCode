7350386cb1ff89eeb3784a7ea113013c7552dc29
潘飞雷 <panfeilei@guanmai.cn>
2020-03-20 14:07:50 +0800

    f

---
+++ 198. 打家劫舍.py
@@ -0,0 +1,30 @@
+def rob(nums):
+    if not nums:
+        return 0
+    if len(nums) <= 2:
+        return max(nums)
+    r = max(nums[:2])
+    max1 = [nums[0],r]
+    index = 2
+    end = len(nums) - 1
+    while  index <= end + 1:
+        if index == end + 1:
+            t = max1[index - 2]
+        else:
+            t = max1[index -2] + nums[index]
+        r = max(r, t)
+        max1.append(r)
+        index += 1
+    return r
+        
+def rob1(nums):
+    premax = 0
+    currentmax = 0
+    for i in nums:
+        tmp = currentmax
+        currentmax = max(premax + i, currentmax)
+        premax = tmp
+    return currentmax
+
+f= rob1([1,1,6,103])
+print(f)
